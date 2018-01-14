2a27fac6d49a38ef79bcbd2fc59cb7c3d06a90ec
blakeharris <harrisblake85@gmail.com>
2018-01-14 16:37:14 -0700

    auth refactor

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,24 +1,19 @@
 class ApplicationController < ActionController::API
 
   def authenticate_token
-    p "AUTHENTICATE JWT"
     render json: { status: 401, message: 'Unauthorized' } unless decode_token(bearer_token)
   end
 
   def bearer_token
-    p "BEARER TOKEN"
-    p header = request.env["HTTP_AUTHORIZATION"]
-
-    pattern= /^Bearer /
-    p "TOKEN WITHOUT BEARER"
-    p header.gsub(pattern, "") if header && header.match(pattern) header.gsub(pattern, "") if header && header.match(pattern)
+    header = request.env['HTTP_AUTHORIZATION']
+    pattern = /^Bearer /
+    header.gsub(pattern, '') if header && header.match(pattern)
   end
 
   def decode_token(token_input)
-    p "DECODE TOKEN, token input: #{token_input}"
     JWT.decode(token_input, ENV['JWT_SECRET'], true)
   rescue
-    render json: {status:401, message:"Unauthorized"}
+    render json: { status: 401, message: 'Unauthorized' }
   end
 
-end
+end 
