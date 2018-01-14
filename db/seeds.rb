1668a011c67780e5ad28fc92445d1b55942e7fdf
carriesampson <i.carrie.sampson@gmail.com>
2018-01-13 19:29:41 -0500

    Added JavaScript Toolies to seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -78,19 +78,121 @@ Language.create(
 Tool.create(
   [
     {
-      title:"For Loop",
+      title:"TESTING: For Loop",
       language_id: 1,
       description: "Repeats a block of code a semi-determined amount of times using an iterator",
       repl_url: "https://repl.it/repls/ConcernedGoldNorthernseahorse",
       num_belts:1
     },
     {
-      title:"For Of Loop",
+      title:"TESTING: For Of Loop",
       language_id: 1,
       description: "Repeats a block of code for every position of an array the iterator taking the value of that position",
       repl_url: "https://repl.it/repls/SizzlingSlimyGenet",
       num_belts:2
-    }
+    },
+      title:"While Loop: Equation",
+      language_id: 1,
+      description: "Returns numbers 1-1000",
+      repl_url: "https://repl.it/@carriesampson/JS-While-Loop-Equation",
+      num_belts:0
+    },
+      title:"While Loop: Boolean",
+      language_id: 1,
+      description: "Changing condition true to false",
+      repl_url: "https://repl.it/@carriesampson/JS-While-Loop-Boolean",
+      num_belts:0
+    },
+      title:"Postfix and Compound Assignment Operators",
+      language_id: 1,
+      description: "Incrementing a number forward or backward with Postfix, and adding/subtracting from a number with Compound Assignment",
+      repl_url: "https://repl.it/@carriesampson/JS-Postfix-and-Compound-Assignment-Operators",
+      num_belts:0
+    },
+      title:"For Loop, Simple Syntax",
+      language_id: 1,
+      description: "Simple For Loop Syntax",
+      repl_url: "https://repl.it/@carriesampson/JS-For-Loop-Simple-Syntax",
+      num_belts:0
+    },
+      title:"If Statement",
+      language_id: 1,
+      description: "Using the If conditional statement",
+      repl_url: "https://repl.it/@carriesampson/JS-If-Statement",
+      num_belts:0
+    },
+      title:"If/Else, Else If Statements",
+      language_id: 1,
+      description: "Using the If/Else and Else If conditional statements",
+      repl_url: "https://repl.it/@carriesampson/JS-IfElse-Else-If-Statements",
+      num_belts:0
+    },
+      title:"For Loop including If/Else Statement",
+      language_id: 1,
+      description: "Using  If/Else conditional statements in a For Loop",
+      repl_url: "https://repl.it/@carriesampson/JS-For-Loop-including-IfElse-Statement",
+      num_belts:0
+    },
+      title:"For Loop including If/Else to Find Even Integers",
+      language_id: 1,
+      description: "Using a For Loop including If/Else Statement to Find Even Integers",
+      repl_url: "https://repl.it/@carriesampson/JS-For-Loop-including-If-Statement-Find-Even-Integers",
+      num_belts:0
+    },
+      title:"For Loop including If/Else to Sum Integers",
+      language_id: 1,
+      description: "Using a For Loop including If/Else Statement to Sum Integers ",
+      repl_url: "https://repl.it/@carriesampson/JS-For-Loop-including-IfElse-Sum-Integers",
+      num_belts:0
+    },
+      title:"Array to Retrieve Item by Index",
+      language_id: 1,
+      description: "Array to Retrieve Item by Index",
+      repl_url: "https://repl.it/@carriesampson/JS-Array-to-Find-Item-by-Index",
+      num_belts:0
+    },
+      title:"For Loop over an Array of Items returning all",
+      language_id: 1,
+      description: "For Loop over an Array returning all Array items",
+      repl_url: "https://repl.it/@carriesampson/JS-For-Loop-over-an-Array-returning-All",
+      num_belts:0
+    },
+      title:"For Loop over an Array returning Every Other Item",
+      language_id: 1,
+      description: "For Loop over an Array returning Every Other Item",
+      repl_url: "https://repl.it/@carriesampson/JS-For-Loop-over-an-Array-returning-Every-Other-Item",
+      num_belts:0
+    },
+      title:"For Loop over an Array returning Even Indices",
+      language_id: 1,
+      description: "Looping over an Array and reurning if the Index is even",
+      repl_url: "https://repl.it/@carriesampson/JS-For-Loop-over-an-Array-returning-Even-Inidices",
+      num_belts:0
+    },
+      title:"Update an Item in an Arrayby Index",
+      language_id: 1,
+      description: "Update an Item in an Array by Index",
+      repl_url: "https://repl.it/@carriesampson/JS-Update-Item-in-Array",
+      num_belts:0
+    },
+      title:"Add an Item in an Array by Index",
+      language_id: 1,
+      description: "Add an Item in an Array by Index",
+      repl_url: "https://repl.it/@carriesampson/JS-Add-Item-to-an-Array-by-Index",
+      num_belts:0
+    },
+      title:"Add an Item in an Array by .push() Method",
+      language_id: 1,
+      description: "Add an Item in an Array by Push Method",
+      repl_url: "https://repl.it/@carriesampson/JS-Adding-to-Array-by-push-Method",
+      num_belts:0
+    },
+      title:"Multi-dimentional Arrays: Using an Array within an Array",
+      language_id: 1,
+      description: "Array elements containing other arrays",
+      repl_url: "https://repl.it/@carriesampson/JS-Adding-to-Array-by-push-Method",
+      num_belts:0
+    },
   ]
 )
 
