6807b5366987233b07449a1ef6abf7af2a151a48
blakeharris <harrisblake85@gmail.com>
2018-01-16 18:54:45 -0700

    Allowing CORS Toolie FrontEnd

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'localhost:3030'
+    origins 'localhost:3030','tooliebox.herokuapp.com'
 
     resource '*',
       headers: :any,
